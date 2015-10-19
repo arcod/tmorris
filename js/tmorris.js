08e3f04f7c1376ca2d0870d2a818240052855441
kenagy.jw <kenagy.jw@gmail.com>
2015-10-19 08:40:54 -0700

    Small Formatting Changes

--- js/tmorris.js
+++ js/tmorris.js
@@ -4,3 +4,20 @@ $('a').click(function(){
     }, 1000);
     return false;
 });
+
+$( window ).load(function() {
+   $('.cover').fadeOut(2000);
+});
+
+$( window ).load(function() {
+   $('.splash h1,h2,h3').fadeIn(500);
+});
+
+
+
+var nav = $('nav');
+$(window).on('scroll', function() {
+	var height = $(window).height();
+	var st = $(this).scrollTop() - height/2;
+    nav.css({ 'opacity' : (0 + st/height*2) });
+});
